fe18b7f4b1d939e4496148d78d66de56b78eb99f
Detbeed <152188603+khaquachtrong74@users.noreply.github.com>
2024-11-20 18:20:01 +0000

    Thử nghiệm với code space của github

--- ou/src/main/java/com/excersice2/Question5/CauHinh.java
+++ ou/src/main/java/com/excersice2/Question5/CauHinh.java
@@ -5,4 +5,5 @@ import java.util.Scanner;
 public class CauHinh {
 
     public final static Scanner SC = new Scanner(System.in);
+    // This file will contains variables const to easily use and check
 }
